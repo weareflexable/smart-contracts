73bf199bde7ec3fb795f5a20f7c0bdb9a30c5ab8
Soumalya Paul <samratghosh240@gmail.com>
2024-09-01 15:13:51 +0530

    Updated the repo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
+require("dotenv").config();
 import dotenv from "dotenv";
 dotenv.config();
+import "@nomicfoundation/hardhat-verify";
+import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
 
 // TESTNET
-const MATIC_RPC_URL =
+const AMOY_RPC_URL =
   process.env.AMOY_RPC_URL || "https://polygon-mumbai.g.alchemy.com/v2/api-key";
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://ETH-RPC-URL";
@@ -13,16 +18,23 @@ const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://ETH-RPC-URL";
 const BASE_TESTNET_RPC_URL =
   process.env.BASE_TESTNET_RPC_URL || "wss://base-sepolia-rpc.publicnode.com";
 
-// PRIVAT/MNEMONIC
+//// MAINNET
+const POLYGON_RPC_URL =
+  process.env.POLYGON_RPC_URL ||
+  "https://polygon-mainnet.g.alchemy.com/v2/api-key";
+
+const BASE_RPC_URL = process.env.BASE_RPC_URL || "https://mainnet.base.org";
+
 const MNEMONIC =
   process.env.MNEMONIC ||
   "ajkskjfjksjkf ssfaasff asklkfl klfkas dfklhao asfj sfk klsfjs fkjs";
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// BLOCK EXPLORER API
 const POLYGONSCAN_API_KEY =
   process.env.POLYGONSCAN_API_KEY || "lklsdkskldjklgdklkld";
+
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Etherscan API key";
+
 const BASESCAN_API_KEY = process.env.BASESCAN_API_KEY || "Basescan API Key";
 
 module.exports = {
@@ -43,39 +55,52 @@ module.exports = {
     // TESTNET NETWORKS
     amoy: {
       networkId: 80002,
-      url: MATIC_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      url: AMOY_RPC_URL,
+      // accounts: [PRIVATE_KEY],
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
     },
     sepolia: {
       networkId: 11155111,
       url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      // accounts: [PRIVATE_KEY],
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
     },
     baseTestnet: {
       networkId: 84532,
       url: BASE_TESTNET_RPC_URL,
+      // accounts : [PRIVATE_KEY],
       accounts: {
         mnemonic: MNEMONIC,
       },
     },
-    //MAINNET
-    matic: {
-      chainId: 137,
-      url: MATIC_RPC_URL,
+    polygon: {
+      networkId: 137,
+      url: POLYGON_RPC_URL,
+      // accounts: [PRIVATE_KEY],
       accounts: {
         mnemonic: MNEMONIC,
       },
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    base: {
+      networkId: 8453,
+      url: BASE_RPC_URL,
+      // accounts: [PRIVATE_KEY],
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+    },
   },
   etherscan: {
     apiKey: {
+      polygon: POLYGONSCAN_API_KEY,
       polygonAmoy: POLYGONSCAN_API_KEY,
       sepolia: ETHERSCAN_API_KEY,
       baseSepolia: BASESCAN_API_KEY,
+      base: BASESCAN_API_KEY,
     },
   },
   paths: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,65 +1,45 @@
-const fs = require("fs");
-const { ethers, run, network } = require("hardhat");
-
-// const jsonContent = JSON.parse(data)
-
-let contractAddress;
-let blockNumber;
-let Verified = false;
+const fs = require("fs")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
-  const SnlFactory = await hre.ethers.getContractFactory("FlexableNFT");
-  const Snl = await SnlFactory.deploy("TokenFest", "TF");
+    const FlexableFactory = await hre.ethers.getContractFactory("FlexableNFTV2")
+    const Flexable = await FlexableFactory.deploy("Flexable", "FLX")
 
-  await Snl.deployed();
+    await Flexable.deployed()
 
-  console.log("FlexableNFT Deployed to:", Snl.address);
-  contractAddress = Snl.address;
-  blockNumber = Snl.provider._maxInternalBlockNumber;
+    console.log("Flexable Contract Deployed to:", Flexable.address)
+    contractAddress = Flexable.address
+    blockNumber = Flexable.provider._maxInternalBlockNumber
 
-  /// VERIFY
-  if (hre.network.name != "hardhat") {
-    await Snl.deployTransaction.wait(6);
-    await verify(Snl.address, ["TokenFest", "TF"]);
-  }
-
-  let chainId;
-
-  if (network.config.chainId != undefined) {
-    chainId = network.config.chainId;
-  } else {
-    chainId = network.config.networkId;
-  }
-
-  console.log(`The chainId is ${chainId}`);
-  const data = { chainId, contractAddress, Verified, blockNumber };
-  const jsonString = JSON.stringify(data);
-  // Log the JSON string
-  console.log(jsonString);
+    /// VERIFY
+    if (hre.network.name != "hardhat") {
+        await Flexable.deployTransaction.wait(6)
+        await verify(Flexable.address, ["Flexable", "FLX"])
+    }
 }
 
 // async function verify(contractAddress, args) {
 const verify = async (contractAddress, args) => {
-  console.log("Verifying contract...");
-  try {
-    await run("verify:verify", {
-      address: contractAddress,
-      constructorArguments: args,
-    });
-    Verified = true;
-  } catch (e) {
-    if (e.message.toLowerCase().includes("already verified")) {
-      console.log("Already Verified!");
-    } else {
-      console.log(e);
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+        Verified = true
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
     }
-  }
-};
+}
 
 // main
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
